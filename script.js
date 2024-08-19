720cc68ed6c1d761075951ca62ab56b71d4ffbb4
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-08-19 15:35:28 +0200

    es1 fix

--- script.js
+++ script.js
@@ -12,15 +12,6 @@ console.log('JS OK');
 //Prendo elemanti dal DOM
 const targetElement = document.getElementById('target');
 
-// Faccio scegliere all'utente un numero da 1 a 6
-let userNumber = parseInt(prompt('Scegli un numero da 1 a 6', 2));
-
-// Validazione
-if (userNumber = '' || isNaN(userNumber) || userNumber > 6) {
-    alert('Valore del campo non valido');
-    location.reload();
-}
-
 // Preparo il lumero casuale da 1 a 6
 const cpuNumber = parseInt(Math.floor(Math.random() * 6) + 1);
 console.log(cpuNumber);
@@ -28,13 +19,23 @@ console.log(cpuNumber);
 // Preparazione messaggio
 let resultMessage = '';
 
-// controllo se l'utente ha vinto
-if (cpuNumber < userNumber) {
-    resultMessage = `Hai vinto! il tuo numero: ${userNumber}, il numero della CPU: ${cpuNumber}`;
-} else if (cpuNumber > userNumber) {
-    resultMessage = `Hai perso! il tuo numero: ${userNumber}, il numero della CPU: ${cpuNumber}`;
+// Faccio scegliere all'utente un numero da 1 a 6
+let userNumber = parseInt(prompt('Scegli un numero da 1 a 6', 2));
+
+// Validazione
+if (userNumber == '' || isNaN(userNumber) || userNumber > 6) {
+    alert('Valore del campo non valido');
+    location.reload();
 } else {
-    resultMessage = `Pareggio! il tuo numero: ${userNumber}, il numero della CPU: ${cpuNumber}`;
+    // controllo se l'utente ha vinto
+    if (cpuNumber < userNumber) {
+        resultMessage = `Hai vinto! il tuo numero: ${userNumber}, il numero della CPU: ${cpuNumber}`;
+    } else if (cpuNumber > userNumber) {
+        resultMessage = `Hai perso! il tuo numero: ${userNumber}, il numero della CPU: ${cpuNumber}`;
+    } else {
+        resultMessage = `Pareggio! il tuo numero: ${userNumber}, il numero della CPU: ${cpuNumber}`;
+    }
+    console.log(resultMessage)
 }
 
 // Stampo in pagina il risultato
